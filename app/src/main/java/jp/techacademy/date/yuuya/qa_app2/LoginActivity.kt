788eb91a2773a32a23daf68c7045cc290b796d34
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-27 15:04:42 +0900

    Initial commit 画像のコピーとconstができていないかも

---
+++ app/src/main/java/jp/techacademy/date/yuuya/qa_app2/LoginActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.date.yuuya.qa_app2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
